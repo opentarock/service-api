f17d393ddbfdb776546ac87d4bb4617583d77f15
Arjan Topolovec <arjan.top@gmail.com>
2014-09-14 20:28:03 +0200

    Repservice now has fallback message type matching on message headers.

--- go/service/repservice.go
+++ go/service/repservice.go
@@ -8,22 +8,31 @@ import (
 	"github.com/opentarock/service-api/go/proto"
 )
 
+type messageHandlers map[proto.Type]MessageHandler
+
 type RepService struct {
 	Address         string
 	socket          *nmsg.RepSocket
-	messageHandlers map[proto.Type]MessageHandler
+	messageHandlers messageHandlers
+	headerHandlers  messageHandlers
 }
 
 func NewRepService(bind string) *RepService {
 	return &RepService{
 		Address:         bind,
-		messageHandlers: make(map[proto.Type]MessageHandler),
+		messageHandlers: make(messageHandlers),
+		headerHandlers:  make(messageHandlers),
 	}
 }
 
 func (s *RepService) AddHandler(messageType proto.Type, handler MessageHandler) {
 	log.Printf("Adding handler for message type: %X", messageType)
 	s.messageHandlers[messageType] = handler
+}
+
+func (s *RepService) AddHeaderHandler(messageType proto.Type, handler MessageHandler) {
+	log.Printf("Adding handler for header message type: %X", messageType)
+	s.headerHandlers[messageType] = handler
 }
 
 func (s *RepService) Start() error {
@@ -55,25 +64,49 @@ func (s *RepService) Start() error {
 				log.Printf("Error parsing message: %s", err)
 				continue
 			}
-			if handler, ok := s.messageHandlers[msg.Type]; ok {
-				response := handler.HandleMessage(msg)
-				responseMsg, err := response.Marshal()
+			if handled, err := s.messageHandlers.handleType(socket, msg.Type, msg); handled {
 				if err != nil {
-					log.Printf("Error marshalling response: %s", err)
+					log.Println(err)
 					continue
 				}
-				responseData, err := responseMsg.Pack()
-				if err != nil {
-					log.Printf("Error packing response message: %s", err)
-					continue
-				}
-				socket.Send(responseData, 0)
 			} else {
-				log.Printf("Unknown message type: %d", recvData[0])
+				var handled bool
+				for msgType, _ := range msg.Header {
+					handled, err = s.headerHandlers.handleType(socket, msgType, msg)
+					if err != nil {
+						log.Println(err)
+						break
+					} else if handled {
+						break
+					}
+				}
+				if !handled {
+					log.Printf("Unknown message type: %d", recvData[0])
+				}
 			}
 		}
 	}()
 	return nil
+}
+
+func (h messageHandlers) handleType(socket *nmsg.RepSocket, msgType proto.Type, msg *proto.Message) (bool, error) {
+	if handler, ok := h[msgType]; ok {
+		response := handler.HandleMessage(msg)
+		responseMsg, err := response.Marshal()
+		if err != nil {
+			return true, fmt.Errorf("Error marshalling response: %s", err)
+		}
+		responseData, err := responseMsg.Pack()
+		if err != nil {
+			return true, fmt.Errorf("Error packing response message: %s", err)
+		}
+		_, err = socket.Send(responseData, 0)
+		if err != nil {
+			return true, fmt.Errorf("Error sending response: %s", err)
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
 func (s *RepService) Close() error {
--- go/service/repservice_test.go
+++ go/service/repservice_test.go
@@ -12,51 +12,119 @@ import (
 	"github.com/opentarock/service-api/go/service"
 )
 
-func TestHandlerIsUsed(t *testing.T) {
+func TestMessageHandler(t *testing.T) {
 	req, err := nmsg.NewReqSocket()
 	assert.Nil(t, err)
 	repService := service.NewRepService("tcp://*:9000")
+	defer repService.Close()
 	req.Connect("tcp://localhost:9000")
 	called := false
 	repService.AddHandler(1,
-		service.MessageHandlerFunc(func(data *proto.Message) *proto.Message {
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
 			called = true
-			return proto.NewMessage(1, []byte{})
+			return proto.CompositeMessage{}
 		}))
 	go func() {
 		repService.Start()
 	}()
 
-	req.Send([]byte{1}, 0)
+	data, err := proto.NewMessage(1, []byte{}).Pack()
+	assert.Nil(t, err)
+	req.Send(data, 0)
 	time.Sleep(100 * time.Millisecond)
 	assert.Equal(t, called, true)
-	defer repService.Close()
 }
 
 func TestOnlyLastAddedHandlerForTypeIsUsed(t *testing.T) {
 	req, err := nmsg.NewReqSocket()
 	assert.Nil(t, err)
-	repService := service.NewRepService("tcp://*:9000")
-	req.Connect("tcp://localhost:9000")
+	repService := service.NewRepService("tcp://*:9001")
+	defer repService.Close()
+	req.Connect("tcp://localhost:9001")
 	calledFirst := false
 	repService.AddHandler(1,
-		service.MessageHandlerFunc(func(data *proto.Message) *proto.Message {
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
 			calledFirst = true
-			return proto.NewMessage(1, []byte{})
+			return proto.CompositeMessage{}
 		}))
 	calledSecond := false
 	repService.AddHandler(1,
-		service.MessageHandlerFunc(func(data *proto.Message) *proto.Message {
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
 			calledSecond = true
-			return proto.NewMessage(1, []byte{})
+			return proto.CompositeMessage{}
 		}))
 	go func() {
 		repService.Start()
 	}()
 
-	req.Send([]byte{1}, 0)
+	data, err := proto.NewMessage(1, []byte{}).Pack()
+	assert.Nil(t, err)
+	req.Send(data, 0)
 	time.Sleep(100 * time.Millisecond)
 	assert.Equal(t, calledFirst, false, "First handler should be owerwritten")
 	assert.Equal(t, calledSecond, true)
+}
+
+func TestHeaderHandler(t *testing.T) {
+	req, err := nmsg.NewReqSocket()
+	assert.Nil(t, err)
+	repService := service.NewRepService("tcp://*:9003")
 	defer repService.Close()
+	req.Connect("tcp://localhost:9003")
+	calledFirst := false
+	repService.AddHandler(1,
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
+			calledFirst = true
+			return proto.CompositeMessage{}
+		}))
+	calledSecond := false
+	repService.AddHeaderHandler(2,
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
+			calledSecond = true
+			return proto.CompositeMessage{}
+		}))
+	go func() {
+		repService.Start()
+	}()
+
+	msg := proto.NewMessage(3, []byte{})
+	msg.Header.Set(2, []byte{})
+	data, err := msg.Pack()
+	assert.Nil(t, err)
+	req.Send(data, 0)
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, calledFirst, false, "Message handler should not be executed")
+	assert.Equal(t, calledSecond, true, "Header handler should be executed")
+}
+
+func TestMessageHandlerHasHigherPriority(t *testing.T) {
+	req, err := nmsg.NewReqSocket()
+	assert.Nil(t, err)
+	repService := service.NewRepService("tcp://*:9004")
+	defer repService.Close()
+	req.Connect("tcp://localhost:9004")
+	calledFirst := false
+	repService.AddHandler(1,
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
+			calledFirst = true
+			return proto.CompositeMessage{}
+		}))
+	calledSecond := false
+	repService.AddHeaderHandler(2,
+		service.MessageHandlerFunc(func(data *proto.Message) proto.CompositeMessage {
+			calledSecond = true
+			return proto.CompositeMessage{}
+		}))
+	go func() {
+		repService.Start()
+	}()
+
+	msg := proto.NewMessage(1, []byte{})
+	msg.Header.Set(2, []byte{})
+	data, err := msg.Pack()
+	assert.Nil(t, err)
+	req.Send(data, 0)
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, calledFirst, true, "Message handler has higher priority and should be called")
+	assert.Equal(t, calledSecond, false, "Header handler is tryed only if there are no handlers for the message")
 }
