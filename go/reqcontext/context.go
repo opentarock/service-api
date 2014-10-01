c4e5275b9db6f9ab1f38f22f6a733cce7c9bbe59
Arjan Topolovec <arjan.top@gmail.com>
2014-10-01 16:01:46 +0200

    Added a function for creating a context specific logger.

--- go/reqcontext/context.go
+++ go/reqcontext/context.go
@@ -1,6 +1,12 @@
 package reqcontext
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	pbuf "code.google.com/p/gogoprotobuf/proto"
+
 	"code.google.com/p/go.net/context"
 	"github.com/opentarock/service-api/go/proto"
 	"github.com/opentarock/service-api/go/proto_headers"
@@ -22,4 +28,12 @@ func NewContext(ctx context.Context, msg *proto.Message, excludeHeaders ...proto
 func CorrIdFromContext(ctx context.Context) (*proto_headers.RequestCorrelationHeader, bool) {
 	h, ok := ctx.Value(reqCorrKey).(proto_headers.RequestCorrelationHeader)
 	return &h, ok
+}
+
+func ContextLogger(ctx context.Context) *log.Logger {
+	corr, ok := CorrIdFromContext(ctx)
+	if !ok {
+		corr.CorrelationId = pbuf.String("unknown")
+	}
+	return log.New(os.Stdout, fmt.Sprintf("[%s] ", corr.GetCorrelationId()), log.Ldate|log.Lmicroseconds)
 }
