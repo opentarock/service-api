6392b0dc7bafd9f8f98d1b99f955e6a6d40613e0
Arjan Topolovec <arjan.top@gmail.com>
2014-10-04 22:34:12 +0200

    Fixed Gcm client to take context as first parameter.

--- go/client/gcm.go
+++ go/client/gcm.go
@@ -1,9 +1,13 @@
 package client
 
-import "github.com/opentarock/service-api/go/proto_gcm"
+import (
+	"code.google.com/p/go.net/context"
+	"github.com/opentarock/service-api/go/proto_gcm"
+)
 
 type GcmClient interface {
 	SendMessage(
+		ctx context.Context,
 		registrationIds []string,
 		data string,
 		params *proto_gcm.Parameters) (*proto_gcm.SendMessageResponse, error)
